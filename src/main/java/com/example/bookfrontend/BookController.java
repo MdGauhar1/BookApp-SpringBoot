e457c8e8ab3bd725475a937c773ed68a8ab6e0f9
gauhar <mdgauhar0007@gmail.com>
2025-04-10 12:58:14 +0530

    a

---
+++ src/main/java/com/example/bookfrontend/BookController.java
@@ -0,0 +1,24 @@
+package com.example.bookfrontend;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+@CrossOrigin(origins = "http://localhost:4200")
+public class BookController {
+    @Autowired
+    private BookRepository repo;
+
+    @GetMapping
+    public List<Book> getAll() { return repo.findAll(); }
+
+    @PostMapping
+    public Book add(@RequestBody Book book) { return repo.save(book); }
+
+    @DeleteMapping("/{id}") public void delete(@PathVariable Long id) { repo.deleteById(id); }
+}
+
---
+++ src/main/java/com/example/bookfrontend/BookfrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookfrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookfrontendApplication.class, args);
+	}
+
+}
