d0b4ba6eb35c81e6215d458a55f1f5c9c8662e17
gauhar <mdgauhar0007@gmail.com>
2025-04-11 10:59:11 +0530

    n1

--- src/main/java/com/example/bookfrontend/BookController.java
+++ src/main/java/com/example/bookfrontend/BookController.java
@@ -3,6 +3,7 @@ package com.example.bookfrontend;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,14 +13,31 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 public class BookController {
     @Autowired
-    private BookRepository repo;
+    private BookRepository bookRepository;
 
     @GetMapping
-    public List<Book> getAll() { return repo.findAll(); }
+    public List<Book> getAll() {
+        return bookRepository.findAll();
+    }
 
     @PostMapping
-    public Book add(@Valid @RequestBody Book book) { return repo.save(book); }
+    public Book add(@Valid @RequestBody Book book) {
+        return bookRepository.save(book);
+    }
 
-    @DeleteMapping("/{id}") public void delete(@PathVariable Long id) { repo.deleteById(id); }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    @GetMapping("/by-title")
+    public ResponseEntity<List<Book>> getBooksByTitle(@RequestParam String title) {
+        return ResponseEntity.ok(bookRepository.findByTitle(title));
+    }
+
+    @GetMapping("/by-author")
+    public ResponseEntity<List<Book>> getBooksByAuthor(@RequestParam String author) {
+        return ResponseEntity.ok(bookRepository.findByAuthor(author));
+    }
 }
 
--- src/main/java/com/example/bookfrontend/BookRepository.java
+++ src/main/java/com/example/bookfrontend/BookRepository.java
@@ -2,5 +2,12 @@ package com.example.bookfrontend;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {}
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByTitle(String title);
+
+    List<Book> findByAuthor(String author);
+}
 
